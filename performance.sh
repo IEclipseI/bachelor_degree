4b668e7c55da7ae7e02d27dfe2ff92403b58d7ca
Roman Smirnov <smirnovromal@yandex-team.ru>
2021-04-21 00:27:25 +0300

    first try

--- performance.sh
+++ performance.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-/usr/lib/jvm/java-11-openjdk-amd64/bin/java -jar target/consoleApp-1.0-SNAPSHOT-jar-with-dependencies.jar
+/usr/lib/jvm/java-11-openjdk-amd64/bin/java -Xmx8g -jar target/consoleApp-1.0-SNAPSHOT-jar-with-dependencies.jar
--- src/main/kotlin/test/PerformanceTests.kt
+++ src/main/kotlin/test/PerformanceTests.kt
@@ -4,8 +4,8 @@ import tests.PerformanceTests
 
 fun main() {
     PerformanceTests(
-        listOf(4, 8),
-        listOf(0.1, 0.2),
-        listOf(1..1000, 1..100_000),
+        listOf(32),
+        listOf(0.1, 0.8),
+        listOf(1..1_000_000),
         seconds = 1).run()
 }
