2462f9a80c9183c27bded121b36ada4b86ebadcd
Roman Smirnov <smirnovromal@yandex-team.ru>
2021-04-21 19:22:02 +0300

    ops counter changed to Long

--- src/main/kotlin/test/PerformanceTests.kt
+++ src/main/kotlin/test/PerformanceTests.kt
@@ -8,7 +8,7 @@ fun main() {
         listOf(32), //9
         listOf(1.0), //5
         listOf(1..1_500_000, 1..5_000_000), //4
-        kArraySizes = listOf(32), //7
+        kArraySizes = listOf(36), //7
         seconds = Duration.ofMinutes(5).toSeconds().toInt(),
         executionsNumber = 1).run()
     println("Finished")
