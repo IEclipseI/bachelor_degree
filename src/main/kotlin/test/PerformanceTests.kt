6cc97d4ab18892a2d263b596e42023161bee7093
Roman <smirnovromal@yandex.ru>
2021-06-01 01:05:59 +0300

    for tests in yandex

--- src/main/kotlin/test/PerformanceTests.kt
+++ src/main/kotlin/test/PerformanceTests.kt
@@ -9,8 +9,8 @@ fun main() {
         listOf(0.2, 0.5, 1.0), //3
         listOf(1..10_000, 1..100_000, 1..500_000, 1..1_500_000, 1..5_000_000, 1..10_000_000), //6
         kArraySizes = listOf(32),
-//        seconds = Duration.ofMinutes(5).toSeconds().toInt(),
-        seconds = Duration.ofSeconds(1).toSeconds().toInt(),
+        seconds = Duration.ofMinutes(5).toSeconds().toInt(),
+//        seconds = Duration.ofSeconds(1).toSeconds().toInt(),
         executionsNumber = 1,
         onlyKArray = false).run()
     println("Finished")
